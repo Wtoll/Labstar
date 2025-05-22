19c7069ce583556da72f49e44b685c7e8344bde4
Will Toll <will@wtoll.com>
2025-05-21 21:41:23 -0700

    Added tokio

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,16 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    labstar_lib::run()
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            tauri::async_runtime::set(tokio::runtime::Handle::current());
+
+            tauri::Builder::default()
+                .plugin(tauri_plugin_opener::init())
+                .run(tauri::generate_context!())
+                .expect("error while running tauri application");
+        });
 }
